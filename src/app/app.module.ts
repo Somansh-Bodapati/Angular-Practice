c91440564c802f8096fdb5eb01251e20fc419a94
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-12-24 17:49:59 -0600

    Changing app structure to use module approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
-    imports: [],
+    imports: [FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
+export class AppModule {}
