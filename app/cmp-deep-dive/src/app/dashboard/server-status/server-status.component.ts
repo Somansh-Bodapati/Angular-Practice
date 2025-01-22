43a642d407ca1f5ae68dc943591005bc70bcd0c4
Somansh-Bodapati <somansh.bodapati@gmail.com>
2025-01-22 13:03:12 -0600

    Added new lifecycle hooks

--- app/cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ app/cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
 
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+    this.interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -24,5 +26,9 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
   }
 }
