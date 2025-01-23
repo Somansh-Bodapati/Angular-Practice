1fa048869974cf68914e4872fa3aa1649bee5c28
Somansh-Bodapati <somansh.bodapati@gmail.com>
2025-01-23 11:18:40 -0600

    Setting up a signal subscription to display servver-status using the effect function and a writable signal

--- app/cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ app/cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,14 +10,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
-  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log("ON INIT");
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log("AFTER VIEW INIT");
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(title: String, ticketText: string){ {
     console.log(title);
     console.log(ticketText);
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
   }
 }
--- app/cmp-deep-dive/src/app/shared/control/control.component.ts
+++ app/cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, afterNextRender, afterRender, Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,7 +12,7 @@ import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostLis
     '(click)': 'onClick()'
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit{
   //@HostBinding('class') className = 'control';
   // @HostListener('click') onClick(){
   //   console.log('Clicked');
@@ -21,6 +21,20 @@ export class ControlComponent {
   private el = inject(ElementRef);
   //@ContentChild('input') private control?: ElementRef<HTMLTextAreaElement | HTMLInputElement>;
   private control = contentChild.required<ElementRef<HTMLTextAreaElement | HTMLInputElement>>('input');
+
+  constructor(){
+    afterRender(() =>{
+      console.log('afterRENDER')
+    });
+
+    afterNextRender(() => {
+      console.log('After next render')
+    })
+  }
+  
+  ngAfterContentInit() {
+    console.log('After Content Init');
+  }
 
   onClick(){
     console.log('Clicked');
