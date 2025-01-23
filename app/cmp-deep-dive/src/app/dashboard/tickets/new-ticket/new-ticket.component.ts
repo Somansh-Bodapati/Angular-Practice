093e1520caf0a315bdff6b3b2a167d8f27c1f471
Somansh-Bodapati <somansh.bodapati@gmail.com>
2025-01-23 09:54:01 -0600

    another viewchild function way of doing the same thing

--- app/cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ app/cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,12 +11,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: String, ticketText: string){ {
     console.log(title);
     console.log(ticketText);
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
   }
 }
